d07ef316cbab2d7479dc8cb4fd2d9ff19370ad00
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-11 13:55:28 +0530

    Create useThrottle.js

---
+++ src/useThrottle.js
@@ -0,0 +1,23 @@
+import { useRef, useState, useEffect } from 'react';
+
+const useThrottle = (value, limit) => {
+  const [throttledValue, setThrottledValue] = useState(value);
+  const lastRan = useRef(Date.now());
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (Date.now() - lastRan.current >= limit) {
+        setThrottledValue(value);
+        lastRan.current = Date.now();
+      }
+    }, limit - (Date.now() - lastRan.current));
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, limit]);
+
+  return throttledValue;
+};
+
+export default useThrottle;
