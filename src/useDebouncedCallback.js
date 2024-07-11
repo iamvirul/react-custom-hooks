b42e2211242b4f8aed3f88cf945dd692730c7bf2
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-11 13:55:18 +0530

    Create useDebouncedCallback.js

---
+++ src/useDebouncedCallback.js
@@ -0,0 +1,21 @@
+import { useRef, useCallback } from 'react';
+
+const useDebouncedCallback = (callback, delay) => {
+  const timer = useRef();
+
+  const debouncedCallback = useCallback(
+    (...args) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
+
+  return debouncedCallback;
+};
+
+export default useDebouncedCallback;
